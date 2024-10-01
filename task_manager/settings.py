2d56e4ef1c9c2a6bfffd62538cf578ccb138c10e
Mickle <putilovms@gmail.com>
2024-10-01 15:39:39 +0400

    add whitenoise module

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,6 +133,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "var/static/"
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
