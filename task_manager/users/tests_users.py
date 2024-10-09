6f11c5841776e3bc06930ad563f03526f4ea4699
Mickle <putilovms@gmail.com>
2024-10-10 00:14:49 +0400

    fix lint

--- task_manager/users/tests_users.py
+++ task_manager/users/tests_users.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.test import RequestFactory
 from django.core.exceptions import ObjectDoesNotExist
 
 
