2695bc32812268f932ba14e7b46b7d9e02bd3e14
Mickle <putilovms@gmail.com>
2024-10-10 23:29:00 +0400

    fix lint

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class Statuses(models.Model):
     name = models.CharField(max_length=30, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
