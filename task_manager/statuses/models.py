6b60c7925b0611023850697b160cf2354f0c3de2
Mickle <putilovms@gmail.com>
2024-10-10 23:27:19 +0400

    added unique name for Statuses model

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 
 class Statuses(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = _('Task status')
