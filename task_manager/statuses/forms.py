17ffefe0a766d2b493ae0af8e3b07ddaf1ae91d3
Mickle <putilovms@gmail.com>
2024-10-10 21:20:50 +0400

    added translit for status_create

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -1,6 +1,6 @@
 from task_manager.statuses.models import Statuses
 from django.forms import ModelForm
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 
 class StatusesForm(ModelForm):
