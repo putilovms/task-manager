68b4a2469c05583c2471d5324239538f53d1e5f3
Mickle <putilovms@gmail.com>
2024-10-11 18:39:59 +0400

    added tasks list view

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,11 +1,27 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from django.contrib.auth.mixins import AccessMixin
+from django.contrib import messages
+from django.views.generic.list import ListView
+from django.urls import reverse_lazy
+from task_manager.tasks.models import Tasks
 
 
-class TasksListView(View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Tasks')
+class LoginRequiredMsgMixin(AccessMixin):
+    redirect_field_name = None
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            text_message = _("You are not logged in! Please log in")
+            messages.error(request, text_message)
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
+
+class TasksListView(LoginRequiredMsgMixin, ListView):
+    login_url = reverse_lazy('login')
+    model = Tasks
+    template_name = 'tasks/tasks_list.html'
 
 
 class TasksCreateView(View):
