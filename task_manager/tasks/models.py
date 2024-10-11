a1fe63c90d4c68dbf2a425c2664e43ff7b9cd742
Mickle <putilovms@gmail.com>
2024-10-11 17:31:41 +0400

    added model Tasks

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,1 +1,21 @@
-# from django.db import models
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
+from task_manager.statuses.models import Statuses
+
+
+class Tasks(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING,
+                               related_name='author')
+    executor = models.ForeignKey(
+        User, on_delete=models.DO_NOTHING, related_name='executor', null=True)
+    status = models.ForeignKey(Statuses, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Task')
