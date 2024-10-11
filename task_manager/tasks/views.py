e83f1cfe1325f26b8d22f27e9614381ca3be67d6
Mickle <putilovms@gmail.com>
2024-10-11 23:00:33 +0400

    added delete task view

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.views import View
+from django.shortcuts import redirect
 from django.contrib.auth.mixins import AccessMixin
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
@@ -9,8 +8,16 @@ from django.utils.translation import gettext_lazy as _
 from task_manager.tasks.models import Tasks
 from .forms import TasksForm
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
-# , DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+
+class AuthorRequiredMixin(AccessMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().author.id != request.user.id:
+            text_message = _("Only the author of the task can delete it")
+            messages.error(request, text_message)
+            return redirect('tasks')
+        return super().dispatch(request, *args, **kwargs)
 
 
 class LoginRequiredMsgMixin(AccessMixin):
@@ -42,9 +49,18 @@ class TasksCreateView(LoginRequiredMsgMixin, SuccessMessageMixin, CreateView):
         return super().form_valid(form)
 
 
-class TasksDeleteView(View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Task delete')
+class TasksDeleteView(
+    LoginRequiredMsgMixin,
+    AuthorRequiredMixin,
+    SuccessMessageMixin,
+    DeleteView
+):
+    login_url = reverse_lazy('login')
+    context_object_name = 'task'
+    model = Tasks
+    template_name = 'tasks/task_delete.html'
+    success_url = reverse_lazy('tasks')
+    success_message = _("The task was successfully deleted")
 
 
 class TasksUpdateView(LoginRequiredMsgMixin, SuccessMessageMixin, UpdateView):
