9b6bd6aa6f8587e97f34552a7e8603c6636168f5
Mickle <putilovms@gmail.com>
2024-10-12 17:27:41 +0400

    fix lint

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -5,10 +5,4 @@ from task_manager.labels import views
 urlpatterns = [
     path('', views.LabelsListView.as_view(),
          name="labels"),
-#     path('create/', views.LabelCreateView.as_view(),
-#          name="label_create"),
-#     path('<int:pk>/delete/', views.LabelDeleteView.as_view(),
-#          name="label_delete"),
-#     path('<int:pk>/update/', views.LabelUpdateView.as_view(),
-#          name="label_update"),
 ]
