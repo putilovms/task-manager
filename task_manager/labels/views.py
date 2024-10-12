5ffa954ce2853944c0dddeef3da31595f1c26cec
Mickle <putilovms@gmail.com>
2024-10-12 17:47:57 +0400

    fix lint

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.contrib import messages
 from django.views.generic.edit import CreateView, DeleteView
-# UpdateView, 
+# UpdateView,
 from .forms import LabelsForm
 
 
