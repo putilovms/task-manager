bc8b1c39c55d0d36f4ff29b1a45dd32a2d7d1e36
Mickle <putilovms@gmail.com>
2024-10-13 23:50:34 +0400

    added wenserver in ALLOWED_HOSTS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'webserver',
                  'python-project-52-afhn.onrender.com']
 
 
