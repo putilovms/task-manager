fa2160b3510741520feb7e845acb834cf69d9cf1
Mickle <putilovms@gmail.com>
2024-10-13 21:16:50 +0400

    fix lint

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -3,8 +3,12 @@ from task_manager.users import views
 
 
 urlpatterns = [
-    path('', views.UsersListView.as_view(), name="users"),
-    path('create/', views.RegistrationView.as_view(), name="user_create"),
-    path('<int:pk>/delete/', views.UserDeleteView.as_view(), name="user_delete"),
-    path('<int:pk>/update/', views.UserUpdateView.as_view(), name="user_update"),
+    path('', views.UsersListView.as_view(),
+         name="users"),
+    path('create/', views.RegistrationView.as_view(),
+         name="user_create"),
+    path('<int:pk>/delete/', views.UserDeleteView.as_view(),
+         name="user_delete"),
+    path('<int:pk>/update/', views.UserUpdateView.as_view(),
+         name="user_update"),
 ]
