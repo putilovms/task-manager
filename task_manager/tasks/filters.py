1aebb1baa283bad3905eba654a3de3ffa52add32
Mickle <putilovms@gmail.com>
2024-10-13 12:52:54 +0400

    added own filtering tasks

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,6 +1,13 @@
 import django_filters
 from task_manager.tasks.models import Tasks
 from task_manager.labels.models import Labels
+from django.forms import CheckboxInput
+from django.utils.translation import gettext_lazy as _
+from django_currentuser.middleware import get_current_user
+
+import logging
+
+log = logging.getLogger(__name__)
 
 
 class MyFilter(django_filters.FilterSet):
@@ -8,7 +15,19 @@ class MyFilter(django_filters.FilterSet):
         field_name='labels',
         queryset=Labels.objects.all(),
     )
+    self_tasks = django_filters.BooleanFilter(
+        field_name='author',
+        widget=CheckboxInput,
+        method='filter_author',
+        label=_('Only your own tasks'),
+    )
+
+    def filter_author(self, queryset, name, value):
+        if value:
+            user = get_current_user()
+            queryset = queryset.filter(author__exact=user.id)
+        return queryset
 
     class Meta:
         model = Tasks
-        fields = ['status', 'executor', 'labels']
+        fields = ['status', 'executor']
